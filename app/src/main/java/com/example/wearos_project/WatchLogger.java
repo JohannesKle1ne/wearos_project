9cbe4ed3a9e421802fb25a55fd8c7323c0f9ea1b
JKle1ne <jojo.kleine@gmx.de>
2020-02-23 15:57:19 +0100

    Added timestamp to Log Files

--- app/src/main/java/com/example/wearos_project/WatchLogger.java
+++ app/src/main/java/com/example/wearos_project/WatchLogger.java
@@ -1,6 +1,9 @@
 package com.example.wearos_project;
 
+import android.os.SystemClock;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 
@@ -10,41 +13,28 @@ public class WatchLogger {
     public static final int SPACE = 1;
     public static final int REMOVE = 2;
     public static final int RESET = 3;
+    public static final int SESSION_STARTED = 4;
 
-
-
+    private long initTime;
     private final String TAG = "WatchLogger";
-
     private int id;
-
-    private ArrayList<String> logs;
+    private ArrayList<LogEntry> logs;
 
 
 
     public WatchLogger(int id){
         this.id = id;
         logs = new ArrayList<>();
-        logs.add("new session started with id: "+id);
+        initTime = SystemClock.elapsedRealtime();
+        logs.add(new LogEntry(SESSION_STARTED,null));
     }
 
     public void log(int type) {
-        switch(type){
-            case (LETTER):
-                logs.add("added letter");
-                break;
-            case (SPACE):
-                logs.add("added space");
-                break;
-            case (REMOVE):
-                logs.add("removed letter");
-                break;
-            case (RESET):
-                logs.add("reset all letters");
-                break;
-            default:
-                Log.d(TAG, "there's no such logger");
-                break;
-        }
+        logs.add(new LogEntry(type,logs.get(logs.size() - 1)));
+    }
+
+    private double getCurrentTimestamp(){
+        return SystemClock.elapsedRealtime()-initTime;
     }
 
     public String getLogs() {
@@ -60,4 +50,50 @@ public class WatchLogger {
     }
 
 
+    private class LogEntry{
+
+        private String logString;
+        private double timestamp;
+        private LogEntry referenceEntry;
+        private int entryType;
+
+        public LogEntry(int entryType, LogEntry referenceEntry) {
+            this.timestamp = getCurrentTimestamp();
+            this.referenceEntry = referenceEntry;
+            this.entryType = entryType;
+
+            switch(entryType){
+                case (SESSION_STARTED): this.logString = "user session started with id: "+id;
+                    break;
+                case (LETTER): this.logString = "added letter";
+                    break;
+                case (SPACE): this.logString = "added space";
+                    break;
+                case (REMOVE): this.logString = "removed Letter";
+                    break;
+                case (RESET): this.logString = "reset all letters";
+                    break;
+                default:
+                    Log.d(TAG, "there's no such entryType");
+                    break;
+            }
+        }
+
+        public double getTimestamp() {
+            return timestamp;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            if(entryType == SESSION_STARTED){
+                return logString = logString + " | time passed: "
+                        + timestamp / 1000 + "s (absolute)";
+            }else {
+                return logString + " | time passed: " + timestamp / 1000 + "s (absolute), "
+                        + (timestamp - referenceEntry.getTimestamp())/1000 + "s (relative)";
+            }
+        }
+    }
 }
+
